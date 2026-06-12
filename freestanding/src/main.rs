86a5e0c7f2263f4a1033e7ee33ca4ad88ee931a6
MSA Studio <mymsastudio@gmail.com>
2026-06-13 06:56:36 +0800

    feat(freestanding B3): IDT-256 with 32 CPU-exception handlers (g11). Per-vector asm stubs (uniform error-code frame) -> common dispatcher saves/restores all GP regs -> exception_handler. Verified in QEMU: int3 (#BP vec 3) routes to handler ('EXC 03'), iretq returns, boot continues to clean exit 33

--- freestanding/src/main.rs
+++ freestanding/src/main.rs
@@ -6,8 +6,6 @@ use core::panic::PanicInfo;
 
 global_asm!(
     r#"
-.intel_syntax noprefix
-
 .section .multiboot_header, "a"
 .align 4
     .long 0x1BADB002
@@ -106,6 +104,107 @@ stack_top:
 "#
 );
 
+global_asm!(
+    r#"
+.section .text
+.code64
+
+.macro ISR_NOERR n
+.global isr\n
+isr\n:
+    push 0
+    push \n
+    jmp isr_common
+.endm
+
+.macro ISR_ERR n
+.global isr\n
+isr\n:
+    push \n
+    jmp isr_common
+.endm
+
+ISR_NOERR 0
+ISR_NOERR 1
+ISR_NOERR 2
+ISR_NOERR 3
+ISR_NOERR 4
+ISR_NOERR 5
+ISR_NOERR 6
+ISR_NOERR 7
+ISR_ERR   8
+ISR_NOERR 9
+ISR_ERR   10
+ISR_ERR   11
+ISR_ERR   12
+ISR_ERR   13
+ISR_ERR   14
+ISR_NOERR 15
+ISR_NOERR 16
+ISR_ERR   17
+ISR_NOERR 18
+ISR_NOERR 19
+ISR_NOERR 20
+ISR_ERR   21
+ISR_NOERR 22
+ISR_NOERR 23
+ISR_NOERR 24
+ISR_NOERR 25
+ISR_NOERR 26
+ISR_NOERR 27
+ISR_NOERR 28
+ISR_ERR   29
+ISR_ERR   30
+ISR_NOERR 31
+
+isr_common:
+    push rax
+    push rcx
+    push rdx
+    push rbx
+    push rbp
+    push rsi
+    push rdi
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+    mov rdi, [rsp + 15*8]
+    mov rsi, [rsp + 16*8]
+    call exception_handler
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rdi
+    pop rsi
+    pop rbp
+    pop rbx
+    pop rdx
+    pop rcx
+    pop rax
+    add rsp, 16
+    iretq
+
+.section .rodata
+.align 8
+.global isr_stub_table
+isr_stub_table:
+    .quad isr0,  isr1,  isr2,  isr3,  isr4,  isr5,  isr6,  isr7
+    .quad isr8,  isr9,  isr10, isr11, isr12, isr13, isr14, isr15
+    .quad isr16, isr17, isr18, isr19, isr20, isr21, isr22, isr23
+    .quad isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31
+"#
+);
+
 const COM1: u16 = 0x3F8;
 
 #[inline]
@@ -140,10 +239,85 @@ fn serial_putc(c: u8) {
 fn serial_puts(s: &[u8]) {
     for &c in s { serial_putc(c); }
 }
+fn hex_digit(n: u8) -> u8 {
+    if n < 10 { b'0' + n } else { b'a' + (n - 10) }
+}
+fn serial_hex2(b: u8) {
+    serial_putc(hex_digit(b >> 4));
+    serial_putc(hex_digit(b & 0x0F));
+}
+
+#[repr(C, packed)]
+#[derive(Clone, Copy)]
+struct IdtEntry {
+    off_low: u16,
+    sel: u16,
+    ist: u8,
+    flags: u8,
+    off_mid: u16,
+    off_high: u32,
+    zero: u32,
+}
+impl IdtEntry {
+    const ZERO: Self = Self {
+        off_low: 0, sel: 0, ist: 0, flags: 0, off_mid: 0, off_high: 0, zero: 0,
+    };
+}
+
+#[repr(C, packed)]
+struct IdtPtr {
+    limit: u16,
+    base: u64,
+}
+
+static mut IDT: [IdtEntry; 256] = [IdtEntry::ZERO; 256];
+
+extern "C" {
+    static isr_stub_table: [u64; 32];
+}
+
+fn make_entry(handler: u64) -> IdtEntry {
+    IdtEntry {
+        off_low: handler as u16,
+        sel: 0x08,
+        ist: 0,
+        flags: 0x8E,
+        off_mid: (handler >> 16) as u16,
+        off_high: (handler >> 32) as u32,
+        zero: 0,
+    }
+}
+
+fn idt_init() {
+    unsafe {
+        let mut v = 0usize;
+        while v < 32 {
+            IDT[v] = make_entry(isr_stub_table[v]);
+            v += 1;
+        }
+        let ptr = IdtPtr {
+            limit: (core::mem::size_of::<[IdtEntry; 256]>() - 1) as u16,
+            base: core::ptr::addr_of!(IDT) as u64,
+        };
+        core::arch::asm!("lidt [{}]", in(reg) &ptr,
+            options(readonly, nostack, preserves_flags));
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn exception_handler(vector: u64, _error: u64) {
+    serial_puts(b"EXC ");
+    serial_hex2(vector as u8);
+    serial_putc(b'\n');
+}
 
 #[no_mangle]
 pub extern "C" fn kmain() -> ! {
     serial_init();
+    serial_puts(b"boot\n");
+    idt_init();
+    serial_puts(b"idt\n");
+    unsafe { core::arch::asm!("int3") };
     serial_puts(b"Logicodex\n");
     unsafe { outb(0xf4, 0x10); }
     loop { unsafe { core::arch::asm!("hlt") }; }
